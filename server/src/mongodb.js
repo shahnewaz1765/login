7c769f81777a470a7094d703e784deff33d75c2c
Shahnewaz <shahnewazchowdhury121@gmail.com>
2024-05-17 04:45:46 +0600

    this is simple

---
+++ server/src/mongodb.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/LogIn")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch((error)=>{
+    console.error("failed to connect",error);
+})
+
+const LogInSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection= new mongoose.model("collection1",LogInSchema)
+
+module.exports=collection
